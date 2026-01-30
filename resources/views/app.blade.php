61deffb1765ec84ab4c02d55f3b4b90a7ff3bcfd
Youllo <youllo@example.com>
2026-01-30 14:13:43 +0700

    install template VDO 02

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="../assets/vendor/fonts/iconify-icons.css">
+    <link rel="stylesheet" href="../assets/vendor/css/core.css">
+    <link rel="stylesheet" href="../assets/css/demo.css">    
+      <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
     
     @vite('resources/css/app.cs')
 
@@ -16,6 +23,12 @@
     <div id="vue-app"></div>
 
     @vite('resources/js/app.js')
+
+     <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+      <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+      <script src="../assets/vendor/js/menu.js"></script>
 
 </body>
 </html>
